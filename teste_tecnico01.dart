aaffbd50e5d7c9412b0788bd233bf889eee5ef17
Rafael Barreto <rafaelbarretomg@gmail.com>
2022-08-17 09:49:26 -0300

    Create teste_tecnico01.dart

---
+++ teste_tecnico01.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+//Programa principal
+void main() {
+  //altere apenas o número dentro dos parenteses.
+  print(somador(20));
+}
+
+//Função que soma todos os números divisiveis por 3 e por 5,
+//qua sao menores do que o numero indicado.
+int somador(int num) {
+  int soma = 0;
+  for (int i = 0; i < num; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      soma += i;
+    }
+  }
+  return soma;
+}
